f5f4989ef6182445eb6367b99c31d370c39d8d8a
Enes Kılıç <eneskilic2710@gmail.com>
2025-10-07 18:29:46 +0300

    homework last

--- main.cpp
+++ main.cpp
@@ -2,7 +2,13 @@
 using namespace std;
 
 // Implement the function
-bool check_prime(int);
+bool check_prime(int n){
+    if (n <= 1) return false;
+    for(int i = 2; i*i <= n; i++){
+        if (n % i == 0) return false;
+    }
+    return true;
+}
 
 int main() {
     cout << (!check_prime(1) ? "Passed Test 1" : "Failed Test 1 !!!") << endl;
